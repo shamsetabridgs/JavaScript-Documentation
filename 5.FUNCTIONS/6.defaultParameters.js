0489dfd8bc2357cc7a6f3a1ad91027b1ac8495a3
shamsetabridgs <ssets333@gmail.com>
2024-09-15 10:31:40 +0600

    Functions DONE

--- 5.FUNCTIONS/6.defaultParameters.js
+++ 5.FUNCTIONS/6.defaultParameters.js
@@ -35,7 +35,7 @@ say('Hello');                    // Hi
 
 
 //....................................................Passing Undefined Arguments.........................................................
-
+/*
 function createDiv(height = '100px', width = '100px', border = 'solid 1px red') {
     let div = document.createElement('div');
     div.style.height = height;
@@ -46,3 +46,89 @@ function createDiv(height = '100px', width = '100px', border = 'solid 1px red') 
 }
 createDiv()
 createDiv(undefined, undefined, 'solid 5px blue');
+*/
+
+
+
+//........................................................Evaluating Default Parameters.....................................................
+/*
+function put(toy, toyBox = []) {
+    toyBox.push(toy);
+    return toyBox;
+}
+console.log(put('Toy Car'));                 // [ 'Toy Car' ]
+console.log(put('Teddy Bear'));              // [ 'Teddy Bear' ]
+*/
+
+/*
+function date( d = today() ) {
+    console.log(d);
+}
+function today() {
+    return (new Date().toLocaleDateString("en-US"));
+}
+date();
+*/
+
+/*
+function requiredArg() {
+    throw new Error('The argument is required');
+}
+function add(x = requiredArg(), y = requiredArg()) {
+    return x + y;
+}
+//add(10);                                 //  error
+console.log(add(10, 20));                  // OK - 30
+*/
+
+
+
+
+
+
+//........................................................Using Others Parameters in Default Values......................................................
+/*
+function add(x = 1, y = x, z = x + y) {
+    return x + y + z;
+}
+console.log(add());                   // 4
+*/
+
+/*
+function subtract( x = y, y = 1 ) {
+    return x - y;
+} 
+subtract(10);                         // Uncaught ReferenceError: Cannot access 'y' before initialization
+*/
+
+
+
+
+
+
+
+//....................................................................Using Functions...................................................................
+/*
+let taxRate = () => 0.1;
+let getPrice = function( price, tax = price * taxRate() ) {
+    return price + tax;
+}
+let fullPrice = getPrice(100);
+console.log(fullPrice);           // 110
+*/
+
+
+
+
+
+
+//...............................................................Arguments Objects.......................................................................
+/*
+function add(x, y = 1, z = 2) {
+    console.log( arguments.length );
+    return x + y + z;
+}
+add(10);                           // 1
+add(10, 20);                       // 2
+add(10, 20, 30);                   // 3
+*/
